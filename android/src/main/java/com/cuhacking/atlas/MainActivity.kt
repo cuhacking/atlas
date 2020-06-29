5bfcf1a38ff90fb3d8f516bb78f6b95508ca10ff
Derek <derek@llamabagel.ca>
2020-06-29 16:28:08 -0400

    Use KMP code in Android app

--- android/src/main/java/com/cuhacking/atlas/MainActivity.kt
+++ android/src/main/java/com/cuhacking/atlas/MainActivity.kt
@@ -2,10 +2,17 @@ package com.cuhacking.atlas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cuhacking.atlas.common.getMessage
+import com.cuhacking.atlas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.exampleText.text = getMessage()
     }
 }
