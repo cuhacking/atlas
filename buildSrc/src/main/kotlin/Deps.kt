22b07aa070e40d03642393c8dd317de17de33610
Safa <safa.howaid@gmail.com>
2020-08-06 07:58:13 -0400

    Update Android Gradle plugin version for compatibility with update to Canary 7

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -2,7 +2,7 @@
 
 object deps {
     object plugins {
-        const val android = "com.android.tools.build:gradle:4.2.0-alpha04"
+        const val android = "com.android.tools.build:gradle:4.2.0-alpha07"
         const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
         const val sqldelight = "com.squareup.sqldelight:gradle-plugin:${Versions.sqldelight}"
         const val buildKonfig = "com.codingfeline.buildkonfig:buildkonfig-gradle-plugin:0.5.1"
