319debb4655c1b57fab6aeb93b47c95a149f3ebd
Safa <safa.howaid@gmail.com>
2020-08-28 16:54:47 -0400

    Upgrade to SQLDelight 1.4.2

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,6 +2,6 @@ object Versions {
     const val kotlin = "1.4.0"
     const val compileSdk = 29
     const val minSdk = 21
-    const val sqldelight = "1.4.1"
+    const val sqldelight = "1.4.2"
     const val ktor = "1.4.0"
 }
