317fa3b3138de02e6f13610a516dc3f8363adb55
Safa <safa.howaid@gmail.com>
2020-08-28 23:27:11 -0400

    Changes based on review

--- common/src/androidTest/kotlin/DatabaseTest.kt
+++ common/src/androidTest/kotlin/DatabaseTest.kt
@@ -7,25 +7,28 @@ import org.junit.Before
 import org.junit.Test
 import kotlin.test.assertTrue
 
-lateinit var driver: SqlDriver
-lateinit var queries: FeatureQueries
-
 class DatabaseTest {
 
+    lateinit var driver: SqlDriver
+    lateinit var database: Database
+
     private val sampleFeature = GeoJsonAdapter.decode("""{"type":"Feature","geometry":{"type":"Point","coordinates":[-75.0,45.0]},"properties":{}}""")
+    private val feature1 = Feature(1, "River Building","Richcraft Hall","Building",null,null,null,sampleFeature)
+    private val feature2 = Feature(2, "RB 2311", null, "room","River Building", "RB2",null,sampleFeature)
+    private val feature3 = Feature(3, "HS 1301B", null, "room","Health Sciences Building", "HS1",null,sampleFeature)
+
+    private fun featureToFTS(feature: Feature) = (Feature_fts(feature.common_name,feature.secondary_name,feature.type,feature.building,feature.floor,feature.search_tags))
 
     @Before
     fun setup() {
         driver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
         Schema.create(driver)
-        val database = driver.createDatabase()
-        queries = database.featureQueries
+        database = driver.createDatabase()
 
-        val feature1 = Feature(1, "River Building","Richcraft Hall","Building",null,null,null,sampleFeature)
-        val feature2 = Feature(2, "RB 2311", null, "room","River Building", "RB2",null,sampleFeature)
-
-        queries.insertFeature(feature1)
-        queries.insertFeature(feature2)
+        database.featureQueries.run {
+            insertFeature(feature1)
+            insertFeature(feature2)
+        }
     }
 
     @After
@@ -35,49 +38,21 @@ class DatabaseTest {
 
     @Test
     fun `check that inserting into feature also inserts into feature_fts`(){
-        val feature3 = Feature(3, "HS 1301B", null, "room","Health Sciences Building", "HS1",null,sampleFeature)
-        queries.insertFeature(feature3)
+        database.featureQueries.insertFeature(feature3)
 
-        assertTrue(queries.search("HS 1301B").executeAsList()
-                .contains(
-                    Feature_fts(
-                            common_name= "HS 1301B",
-                            secondary_name= null,
-                            type= "room",
-                            building= "Health Sciences Building",
-                            floor= "HS1",
-                            search_tags= null
-                    )
-
-                )
+        assertTrue(database.featureQueries.search("HS 1301B").executeAsList()
+                .contains(featureToFTS(feature3))
         )
     }
 
     @Test fun `check that search query works`(){
-        assertTrue(queries.search("river").executeAsList().containsAll(
-                listOf(
-                        Feature_fts(
-                            common_name= "River Building",
-                            secondary_name= "Richcraft Hall",
-                            type= "Building",
-                            building= null,
-                            floor= null,
-                            search_tags= null
-                        ),
-                        Feature_fts(
-                                common_name= "RB 2311",
-                                secondary_name= null,
-                                type= "room",
-                                building= "River Building",
-                                floor= "RB2",
-                                search_tags= null
-                        )
-                )
+        assertTrue(database.featureQueries.search("river").executeAsList().containsAll(
+                listOf(featureToFTS(feature1), featureToFTS(feature2))
         ))
     }
 
     @Test fun `check that clearing feature_fts works`(){
-        queries.clearFts()
-        assertTrue(queries.search("river").executeAsOneOrNull() == null)
+        database.featureQueries.clearFts()
+        assertTrue(database.featureQueries.search("river").executeAsOneOrNull() == null)
     }
 }
