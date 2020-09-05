e8ecfbe88dcdfb2095d885709dcb8a05fba031b0
Graydon Haskins <43185853+ghaskins99@users.noreply.github.com>
2020-09-04 23:05:46 -0400

    add common implementation of geojson data source, with feature using … (#40)
    * add common implementation of geojson data source, with feature using it on the map
    
    * deal with detek-ed issues
    
    * move geojson-related to mapbox package
    
    * allow spatialk/geojson to be resolved from android

---
+++ mapbox/src/commonMain/kotlin/com/cuhacking/mapbox/GeoJsonSource.kt
@@ -0,0 +1,18 @@
+package com.cuhacking.mapbox
+
+import io.github.dellisd.spatialk.geojson.Feature
+import io.github.dellisd.spatialk.geojson.FeatureCollection
+import io.github.dellisd.spatialk.geojson.Geometry
+
+expect class GeoJsonSource(id: String, features: FeatureCollection?) {
+
+    val id: String
+
+    fun setGeoJson(json: String)
+
+    fun setGeoJson(feature: Feature)
+
+    fun setGeoJson(geometry: Geometry)
+
+    fun setGeoJson(featureCollection: FeatureCollection)
+}
---
+++ mapbox/src/jsMain/kotlin/com/cuhacking/mapbox/GeoJsonSource.kt
@@ -0,0 +1,32 @@
+package com.cuhacking.mapbox
+
+import io.github.dellisd.spatialk.geojson.Feature
+import io.github.dellisd.spatialk.geojson.FeatureCollection
+import io.github.dellisd.spatialk.geojson.Geometry
+
+
+@ExperimentalJsExport
+@JsExport
+actual class GeoJsonSource actual constructor(actual val id: String, features: FeatureCollection?) {
+    val type = "geojson"
+    var data: dynamic = null
+
+    actual fun setGeoJson(json: String) {
+        data = JSON.parse(json)
+    }
+
+    @JsName("setGeoJsonFeature")
+    actual fun setGeoJson(feature: Feature) {
+        data = JSON.parse(feature.json)
+    }
+
+    @JsName("setGeoJsonGeometry")
+    actual fun setGeoJson(geometry: Geometry) {
+        data = JSON.parse(geometry.json)
+    }
+
+    @JsName("setGeoJsonFeatureCollection")
+    actual fun setGeoJson(featureCollection: FeatureCollection) {
+        data = JSON.parse(featureCollection.json)
+    }
+}
