0ada1e0f9f4fcaba54d093ece59e9c48ff850736
Safa <safa.howaid@gmail.com>
2020-09-26 00:16:48 -0400

    Update DatabaseTest to fix failing tests

--- common/src/androidTest/kotlin/DatabaseTest.kt
+++ common/src/androidTest/kotlin/DatabaseTest.kt
@@ -14,11 +14,11 @@ class DatabaseTest {
     lateinit var database: Database
 
     private val sampleFeature = GeoJsonAdapter.decode("""{"type":"Feature","geometry":{"type":"Point","coordinates":[-75.0,45.0]},"properties":{}}""")
-    private val feature1 = Feature(1, "River Building","Richcraft Hall","Building",null,null,null,sampleFeature)
-    private val feature2 = Feature(2, "RB 2311", null, "room","River Building", "RB2",null,sampleFeature)
-    private val feature3 = Feature(3, "HS 1301B", null, "room","Health Sciences Building", "HS1",null,sampleFeature)
+    private val feature1 = Feature(1, "River Building", "Richcraft Hall", "Building", null, null, null, sampleFeature)
+    private val feature2 = Feature(2, "RB 2311", null, "room", "River Building", "RB2", null, sampleFeature)
+    private val feature3 = Feature(3, "HS 1301B", null, "room", "Health Sciences Building", "HS1", null, sampleFeature)
 
-    private fun Feature.toFTS() = (Feature_fts(common_name,secondary_name,type,building,floor,search_tags))
+    private fun Feature.toFTS() = (Feature_fts(common_name, secondary_name, type, building, floor, search_tags))
 
     @Before
     fun setup() {
@@ -27,18 +27,20 @@ class DatabaseTest {
         database = driver.createDatabase()
 
         database.featureQueries.run {
+            deleteAll()
+            clearFts()
             insertFeature(feature1)
             insertFeature(feature2)
         }
     }
 
     @After
-    fun close(){
+    fun close() {
         driver.close()
     }
 
     @Test
-    fun `check that inserting into feature also inserts into feature_fts`(){
+    fun `check that inserting into feature also inserts into feature_fts`() {
         database.featureQueries.insertFeature(feature3)
 
         assertTrue(database.featureQueries.search("HS 1301B").executeAsList()
@@ -46,13 +48,13 @@ class DatabaseTest {
         )
     }
 
-    @Test fun `check that search query works`(){
+    @Test fun `check that search query works`() {
         assertTrue(database.featureQueries.search("river").executeAsList().containsAll(
                 listOf(feature1.toFTS(), feature2.toFTS())
         ))
     }
 
-    @Test fun `check that clearing feature_fts works`(){
+    @Test fun `check that clearing feature_fts works`() {
         database.featureQueries.clearFts()
         assertTrue(database.featureQueries.search("river").executeAsOneOrNull() == null)
     }
