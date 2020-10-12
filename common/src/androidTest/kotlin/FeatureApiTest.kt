e436f17bc81cb15b8d5773ef50f5e84bbe134dea
Safa <safa.howaid@gmail.com>
2020-10-12 18:40:14 -0400

    Fix detekt warnings

--- common/src/androidTest/kotlin/FeatureApiTest.kt
+++ common/src/androidTest/kotlin/FeatureApiTest.kt
@@ -1,16 +1,21 @@
 import com.cuhacking.atlas.common.FeatureApi
-import com.cuhacking.atlas.db.*
+import com.cuhacking.atlas.db.Database
+import com.cuhacking.atlas.db.GeoJsonAdapter
+import com.cuhacking.atlas.db.Feature
+import com.cuhacking.atlas.db.createDatabase
 import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
-import io.ktor.client.*
-import io.ktor.client.engine.mock.*
-import io.ktor.http.*
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.mock.MockEngine
+import io.ktor.client.engine.mock.respond
+import io.ktor.http.HttpStatusCode
 import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Before
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
+@Suppress("MaxLineLength")
 class FeatureApiTest {
 
     lateinit var driver: SqlDriver
@@ -56,7 +61,7 @@ class FeatureApiTest {
     private val feature1 = Feature(1, "sample name", null, "sample type", null, null, "sample name", feature1Json)
     private val feature2Json = GeoJsonAdapter.decode("""{"type":"Feature","geometry":{"type":"Point","coordinates":[-75.0,45.0]},"properties":{"id":2,"name":"sample name","type":"sample type","building":"building","floor":"floor"}}""")
     private val feature2 = Feature(2, "sample name", null, "sample type", "building", "floor", "sample name", feature2Json)
-    private val featureList = mutableListOf(feature1,feature2)
+    private val featureList = mutableListOf(feature1, feature2)
 
     private val client = HttpClient(MockEngine) {
         engine {
@@ -87,6 +92,6 @@ class FeatureApiTest {
     fun `check that data is downloaded and inserted to database`() = runBlocking {
         FeatureApi(database, client).getAndStoreFeatures()
 
-        assertEquals(database.featureQueries.getAll().executeAsList(),featureList)
+        assertEquals(database.featureQueries.getAll().executeAsList(), featureList)
     }
 }
--- common/src/commonMain/kotlin/com/cuhacking/atlas/common/FeatureApi.kt
+++ common/src/commonMain/kotlin/com/cuhacking/atlas/common/FeatureApi.kt
@@ -4,28 +4,27 @@ import com.cuhacking.atlas.db.Feature as DbFeature
 import com.cuhacking.atlas.db.Database
 import io.github.dellisd.spatialk.geojson.Feature
 import io.github.dellisd.spatialk.geojson.FeatureCollection.Companion.toFeatureCollection
-import io.ktor.client.*
-import io.ktor.client.request.*
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
 import kotlinx.coroutines.withContext
 
-class FeatureApi (private val database: Database, private val client: HttpClient) {
+class FeatureApi(private val database: Database, private val client: HttpClient) {
 
     private fun Feature.toDbFeature() = DbFeature(
         properties["id"].toString().toLong(),
         properties["name"].toString().replace("\"", ""),
         null,
         properties["type"].toString().replace("\"", ""),
-        properties["building"].toString().replace("\"", ""). takeIf { it != "null" },
-        properties["floor"].toString().replace("\"", ""). takeIf { it != "null" },
+        properties["building"].toString().replace("\"", "").takeIf { it != "null" },
+        properties["floor"].toString().replace("\"", "").takeIf { it != "null" },
         properties["name"].toString().replace("\"", ""),
         this)
 
     suspend fun getAndStoreFeatures() = withContext(CoroutineDispatchers.io) {
-        val features : List<Feature> = client.get<String>(AtlasConfig.SERVER_URL).toFeatureCollection().features
+        val features: List<Feature> = client.get<String>(AtlasConfig.SERVER_URL).toFeatureCollection().features
 
         features.forEach {
             database.featureQueries.insertFeature(it.toDbFeature())
         }
     }
 }
-
