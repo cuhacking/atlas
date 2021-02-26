22d1ea3bd968cad9bc645b0c922eab0e6ac31a6c
Safa <safa.howaid@gmail.com>
2021-02-25 20:28:53 -0500

    Initial commit

--- android/src/main/java/com/cuhacking/atlas/MainActivity.kt
+++ android/src/main/java/com/cuhacking/atlas/MainActivity.kt
@@ -34,7 +34,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContent {
+            DefaultPreview()
+        }
+//        setContentView(binding.root)
 
         binding.mapView.onCreate(savedInstanceState)
 
