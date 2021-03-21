842de3358f2ce93a7999fb7b7bc9381746dddd7c
Derek <derek@llamabagel.ca>
2021-03-21 19:12:53 -0400

    Implement basic search in web project
    Fix tests
    
    Remove GeoJsonAdapterTest

--- common/src/androidTest/kotlin/com/cuhacking/atlas/db/TestDriver.kt
+++ common/src/androidTest/kotlin/com/cuhacking/atlas/db/TestDriver.kt
@@ -3,4 +3,7 @@ package com.cuhacking.atlas.db
 import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
 
-actual suspend fun createTestDbDriver(): SqlDriver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
+actual suspend fun createTestDbDriver(): SqlDriver =
+    JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY).also { driver ->
+        AtlasDatabase.Schema.create(driver)
+    }
--- common/src/jsTest/kotlin/com/cuhacking/atlas/db/TestDriver.kt
+++ common/src/jsTest/kotlin/com/cuhacking/atlas/db/TestDriver.kt
@@ -4,4 +4,4 @@ import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.drivers.sqljs.initSqlDriver
 import kotlinx.coroutines.await
 
-actual suspend fun createTestDbDriver(): SqlDriver = initSqlDriver().await()
+actual suspend fun createTestDbDriver(): SqlDriver = initSqlDriver(AtlasDatabase.Schema).await()
