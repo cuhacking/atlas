8325e2e8de36189307f701fbaa998c5f47809091
Safa <safa.howaid@gmail.com>
2021-05-30 14:34:53 -0400

    Bring this branch back to life

--- android/src/main/java/com/cuhacking/atlas/MainActivity.kt
+++ android/src/main/java/com/cuhacking/atlas/MainActivity.kt
@@ -3,9 +3,21 @@ package com.cuhacking.atlas
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
+import androidx.lifecycle.lifecycleScope
+import com.cuhacking.atlas.common.CoroutineDispatchers
+import com.cuhacking.atlas.common.SearchViewModel
+import com.cuhacking.atlas.db.sharedDatabase
 import com.cuhacking.atlas.ui.MainLayout
 
 class MainActivity : AppCompatActivity() {
+    val searchViewModel by lazy {
+        SearchViewModel(
+            CoroutineDispatchers,
+            lifecycleScope,
+            sharedDatabase
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
