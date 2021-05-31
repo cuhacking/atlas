39395b560e9884811d5ba91da30d3a65f0697073
Derek <derek@llamabagel.ca>
2021-05-30 23:51:29 -0400

    Upgrade npm packages
    Apply light style to web map

--- web/src/Map.tsx
+++ web/src/Map.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactMapGL, { ViewState, Source, Layer } from "react-map-gl";
+import ReactMapGL, { ViewportProps, Source, Layer } from "react-map-gl";
 import { AutoSizer } from "react-virtualized";
 import "mapbox-gl/dist/mapbox-gl.css";
 
@@ -9,21 +9,23 @@ import exampleDataSource = com.cuhacking.atlas.common.exampleDataSource;
 import exampleLayer = com.cuhacking.atlas.common.exampleLayer;
 
 export const Map = () => {
-  const [viewport, setViewport] = React.useState<ViewState>({
+  const [viewport, setViewport] = React.useState<ViewportProps>({
     longitude: -75.696127,
     latitude: 45.386438,
     zoom: 14.661,
   });
 
-  const {sourceId, ...layer} = exampleLayer.toJsObject();
+  const { sourceId, ...layer } = exampleLayer.toJsObject();
 
   return (
     <AutoSizer>
       {({ width, height }: { width: number; height: number }) => (
         <ReactMapGL
+          mapStyle="mapbox://styles/mapbox/light-v10"
           width={width}
           height={height}
-          viewState={viewport}
+          {...viewport}
+          scrollZoom={{ speed: 1, smooth: true } as unknown as boolean}
           onViewportChange={setViewport}
           mapboxApiAccessToken={AtlasConfig.MAPBOX_KEY}
         >
