cbe4d3b154312eed0b1d006cbbc559555bdaab5e
Derek <derek@llamabagel.ca>
2021-06-14 23:13:52 -0400

    Add subscription to data flow from Swift

--- common/src/iosMain/kotlin/com/cuhacking/atlas/common/CoroutineDispatchers.kt
+++ common/src/iosMain/kotlin/com/cuhacking/atlas/common/CoroutineDispatchers.kt
@@ -1,27 +1,14 @@
 package com.cuhacking.atlas.common
 
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Runnable
-import platform.darwin.dispatch_async
-import platform.darwin.dispatch_get_main_queue
-import platform.darwin.dispatch_queue_t
-import kotlin.coroutines.CoroutineContext
-import kotlin.native.concurrent.freeze
+import kotlinx.coroutines.Dispatchers
 
 /**
  * At the moment, Kotlin/Native doesn't really support multi-threaded coroutines,
  * so we'll just stick with the Main thread for all coroutines
  */
 actual object CoroutineDispatchers {
-    actual val main: CoroutineDispatcher = NsQueueDispatcher(dispatch_get_main_queue())
-    actual val io: CoroutineDispatcher = NsQueueDispatcher(dispatch_get_main_queue())
-    actual val computation: CoroutineDispatcher = NsQueueDispatcher(dispatch_get_main_queue())
-}
-
-internal class NsQueueDispatcher(private val dispatchQueue: dispatch_queue_t) : CoroutineDispatcher() {
-    override fun dispatch(context: CoroutineContext, block: Runnable) {
-        dispatch_async(dispatchQueue.freeze()) {
-            block.run()
-        }
-    }
+    actual val main: CoroutineDispatcher = Dispatchers.Main
+    actual val io: CoroutineDispatcher = Dispatchers.Default
+    actual val computation: CoroutineDispatcher = Dispatchers.Default
 }
--- ios/atlas/MapView.swift
+++ ios/atlas/MapView.swift
@@ -9,12 +9,13 @@
 import Common
 import SwiftUI
 import Mapbox
+import os
 
 struct MapView: UIViewRepresentable {
 
     private let mapView: MGLMapView = MGLMapView(frame: .zero, styleURL: MGLStyle.darkStyleURL)
 
-    private let searchViewModel: SearchViewModelKt = SearchViewModelKt.provideSearchViewModel()
+    private let searchViewModel: SearchViewModel = SearchViewModelKt.provideSearchViewModel()
 
     func makeUIView(context: UIViewRepresentableContext<MapView>) -> MGLMapView {
         mapView.delegate = context.coordinator
@@ -25,7 +26,7 @@ struct MapView: UIViewRepresentable {
     }
     
     func makeCoordinator() -> MapView.Coordinator {
-        Coordinator(self)
+        Coordinator(self, searchViewModel)
     }
 
     func styleURL(_ styleURL: URL) -> MapView {
@@ -45,19 +46,36 @@ struct MapView: UIViewRepresentable {
     
     final class Coordinator: NSObject, MGLMapViewDelegate {
         var control: MapView
+        var viewModel: SearchViewModel
 
-        init(_ control: MapView) {
+        init(_ control: MapView, _ viewModel: SearchViewModel) {
             self.control = control
+            self.viewModel = viewModel
         }
 
         func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
 
-            let source = DataSourceKt.exampleDataSource
-            mapView.style?.addSource(source.internalSource)
-
-            let layer = DataSourceKt.exampleLayer.getMglLayer(source: source.internalSource)
-            mapView.style?.addLayer(layer)
-
+            viewModel.dataSource.subscribe { (source: MmappGeoJsonSource?) in
+                guard let source = source else { return }
+                
+                os_log("Loaded data: %@", source)
+                
+                if let oldSource = mapView.style?.source(withIdentifier: source.id) {
+                    mapView.style?.removeSource(oldSource)
+                }
+                mapView.style?.addSource(source.internalSource)
+                
+                if let oldLayer = mapView.style?.layer(withIdentifier: DataSourceKt.exampleLayer.id) {
+                    mapView.style?.removeLayer(oldLayer)
+                }
+                
+                let layer = DataSourceKt.exampleLayer.getMglLayer(source: source.internalSource)
+                mapView.style?.addLayer(layer)
+            } onError: { (e: KotlinThrowable) in
+                os_log("Error: %@", e)
+            } onComplete: {
+                
+            }
         }
 
         func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? {
